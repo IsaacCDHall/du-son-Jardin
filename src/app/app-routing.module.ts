60826622be81dfd0978f92d2534ca1ef77cc3421
Isaac Hall <isaac@>
2019-04-10 09:01:43 -0700

    Route page redirection protection. splash page logo class

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SplashPageComponent } from './splash-page/splash-page.component';
+import { PaperCanvasComponent } from './paper-canvas/paper-canvas.component';
+import { KeysComponent } from './keys/keys.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/splash', pathMatch: 'full' },
+  { path: 'splash', component: SplashPageComponent },
+  { path: 'paper', component: PaperCanvasComponent },
+  { path: 'keys', component: KeysComponent },
+  { path: '**', redirectTo: 'splash' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
